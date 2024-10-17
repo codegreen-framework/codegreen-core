e1dad9be1b75c8804700733569bd3202047484a5
shubh <shubhvjain@gmail.com>
2024-10-17 18:54:11 +0200

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codegreen_core',
-    version='0.1',
+    version='0.5.0',
     include_package_data=True,
     package_data={
         'codegreen_core.utilities': ['country_list.json','ci_default_values.csv','model_details.json'],
